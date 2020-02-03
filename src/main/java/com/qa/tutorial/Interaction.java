bd45ad988c189401045107a560e8e192ab7f1d64
Johannes <Johannes.Bull@academytrainee.com>
2020-02-03 14:33:10 +0000

    Project Maven Tutorial

---
+++ src/main/java/com/qa/tutorial/Interaction.java
@@ -0,0 +1,21 @@
+package com.qa.tutorial;
+
+public class Interaction {
+	
+	/**
+	 * This method is for people you like
+	 * @return a greeting
+	 */
+	public String greeting() {
+		return "Hi friend";
+	}
+	
+	/** 
+	 * This method is for people in your class
+	 * @return aninsult
+	 */
+	public String insult() {
+		return "is trash";
+	}
+
+}
